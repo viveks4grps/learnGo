243647a3c2e86c361d20399f101fa56d24422a96
Vivek Sreevatsan (vivsreev) <vivsreev@cisco.com>
2024-10-30 16:57:10 +0530

    Arrays

--- trials/getSizeDataTypes.go
+++ trials/getSizeDataTypes.go
@@ -14,6 +14,7 @@ func main() {
 	f1 := float32(1234.0)
 	//Scientific notation
 	f2 := 1.2345e-2
+	const aConstant = 10
 
 	fmt.Printf("***I AM INSIDE getSizeDataTypes.go FILE***\n")
 	fmt.Printf("x: %T :%d bytes\n",x,unsafe.Sizeof(x))
@@ -24,6 +25,7 @@ func main() {
 	fmt.Printf("f2: %T :%d bytes\n",f2, unsafe.Sizeof(f2))
 	fmt.Printf("f2: %f\n",f2)
 	fmt.Printf("ASCII value of A=%d\n",'A')
+	fmt.Printf("Constant Value=%d\n",aConstant)
 
 	/* Max of types*/
 	fmt.Printf("max float32 = %f\n", math.MaxFloat32)
@@ -53,17 +55,85 @@ func main() {
 
 	//Test Arrays
 	learnArrays()
+
+	//Test Strings
+	exploreStrings()
 }
 
 func learnArrays() {
 	var unsetArray [5]int 
+	//Easy way of traversal using range expression
+	fmt.Println("Using a range Expression based for loop for array traversal:")
 	for index,item := range unsetArray {
 		fmt.Printf("Index:%d is having:%d\n",index,item)
 	}
 
-	var initializedArray [5]int = [5]int{1,2,3,4,5}
+	//Another way of traversal using the normal for loop:
+	fmt.Println("Using a normal for loop for array traversal:")
+	for index:=0; index < len(unsetArray); index++ {
+		fmt.Printf("Index:%d is having:%d\n",index,unsetArray[index])
+	}
+
+	/*
+	//syntax error: unexpected {, expected type
+	var arrayLiteralWithInferredType [2]int = [2]{3,4}
+	*/
+
+	var initializedArray [6]int = [6]int{1,2,3,4,5,6}
+	fmt.Println("Using an array LITERAL, during Initialization:")
 	for index,item := range initializedArray {
 		fmt.Printf("Index:%d is having:%d\n",index,item)
 	}
+
+	//How about using the KEY-WORD "..."
+	/* An ellipsis is a series of three dots (...) used to indicate the 
+	deliberate omission of a word or words 
+	from a sentence without preventing the meaning from being understood. 
+	It can also imply a pause or trail off in thought.
+	*/
+	var arrWithEllipsis [2]int = [...]int{3,4}
+	fmt.Println("Array with ellipsis:")
+	for index,item := range arrWithEllipsis {
+		fmt.Println("index:",index,"item:",item)
+	}
+
+}
+
+func exploreStrings() {
+
+	simpleString := "abcde"
+	fmt.Println("simpleString:",simpleString)
+	for i := 0; i < len(simpleString); i++ {
+        fmt.Printf("%x ", simpleString[i])
+    }
+    fmt.Println("");
+
+	s := "ceлёдка" //"селёдка"
+	fmt.Println("String s:",s)
+	for i:=0; i < len(s); i++ {
+		fmt.Printf("%d: %x ",i,s[i])
+	}
+	fmt.Println("");
+	nonAscii := "л"
+	fmt.Println("Len of nonAscii:",len(nonAscii))
+
+	//Rune is a Unicode character
+	runeArray := []rune(s)
+	fmt.Println("RuneArray:");
+	for i:=0; i < len(runeArray); i++ {
+		fmt.Printf("%d: %c ",i,runeArray[i])
+	}
+	fmt.Println("");
+
+	//Extracting runes from String using range function
+	const nihongo = "日本語"
+    for index, runeValue := range nihongo {
+        fmt.Printf("%#U starts at byte position %d\n", runeValue, index)
+    }
+
+    for index,runeChar := range s {
+		fmt.Printf("Position:%d  Character: %c\n",index,runeChar)
+	}
+	
 
 }
